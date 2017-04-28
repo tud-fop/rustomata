2467c5a817fff670b6c85c2c94e12a29c7004c99
Nogodful <kornmaxhohnstein@gmail.com>
2017-04-28 11:10:35 +0200

    Changed cfg grammars
    Cfgs now use the same grammar as mcfg, but only allow one initial and one element of composition per rule

--- src/cfg/from_str.rs
+++ src/cfg/from_str.rs
@@ -1,132 +1,63 @@
+use std::fmt::Debug;
 use std::marker::PhantomData;
 use std::str::FromStr;
 
+pub use pmcfg::*;
 use cfg::{LetterT, Composition, CFGRule, CFG};
 
-impl<N: FromStr, T: FromStr + Clone, W: FromStr> FromStr for CFG<N, T, W> {
+impl<N: FromStr + Clone, T: FromStr + Clone, W: FromStr + Clone> FromStr for CFG<N, T, W>
+    where <N as FromStr>::Err: Debug, <T as FromStr>::Err: Debug, <W as FromStr>::Err: Debug {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let initial: N;
-        let mut rules: Vec<CFGRule<N, T, W>> = Vec::new();
+        let pmcfg : PMCFG<N, T, W> = s.parse().unwrap();
+        let mut rules = Vec::new();
 
-        let mut it = s.lines();
-
-        match it.next() {
-            Some(l) if l.starts_with("initial: ") => {
-                initial = try!(l[8..]
-                    .trim()
-                    .parse()
-                    .map_err(|_| format!("Substring {} is not a nonterminal.", l[8..].trim())))
-            }
-            _ => return Err("No initial nonterminal supplied on line 1.".to_string()),
+        if pmcfg.initial.len()!=1{
+            return Err(format!("Too many initials"))
         }
 
-        for l in s.lines() {
-            if !l.is_empty() && !l.starts_with("initial: ") {
-                rules.push(try!(l.trim().parse()));
+        for r in pmcfg.rules{
+            if r.composition.composition.len()!=1{
+                return Err(format!("Too many ......")) //TODO name
             }
+
+            let mut new_composition = Vec::new();
+
+            let r_0 = &r.composition.composition[0];
+
+            for v in r_0{
+                match v{
+                    &VarT::T(ref x)=>{
+                        new_composition.push(
+                            LetterT::Value(x.clone())
+                        );
+                    },
+                    &VarT::Var(i,0)=>{
+                        let x = &r.tail[i as usize];
+                        new_composition.push(
+                            LetterT::Label(x.clone())
+                        );
+                    },
+                    _=> return Err(format!("Could not compile rules")), //TODO name
+                }
+
+            }
+            rules.push(CFGRule{
+                head: r.head.clone(),
+                composition: Composition{
+                    composition: new_composition,
+                },
+                weight: r.weight.clone(),
+            });
         }
+
 
         Ok(CFG {
             _dummy: PhantomData,
-            initial: initial,
+            initial: pmcfg.initial[0].clone(),
             rules: rules,
         })
 
-    }
-}
-
-impl<N: FromStr, T: FromStr + Clone, W: FromStr> FromStr for CFGRule<N, T, W> {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let head: N;
-
-        match (s.contains('→'), s.contains("->"), s.split('→').nth(0), s.split("->").nth(0)) {
-            (true, false, Some(v), _) => {
-                head = try!(v.trim().parse().map_err(|_| format!("Can not parse \"{}\" as N.", v)))
-            }
-            (false, true, _, Some(v)) => {
-                head = try!(v.trim().parse().map_err(|_| format!("Can not parse \"{}\" as N.", v)))
-            }
-            _ => return Err(format!("No unique arrow found in \"{}\".", s)),
-        }
-
-        let composition: Composition<N,T>;
-
-        match (s.find('['), s.rfind(']')) {
-            (Some(l), Some(r)) => composition = try!(s[l..r + 1].parse()),
-            _ => return Err(format!("No composition function found in \"{}\".", s)),
-        }
-
-        let weight: W;
-
-        match s.rfind('#') {
-            Some(pos) => {
-                weight = try!(s[pos + 1..]
-                    .trim()
-                    .parse()
-                    .map_err(|_| format!("Can not parse \"{}\" as W.", &s[pos + 1..])))
-            }
-            _ => return Err(format!("No weight found in \"{}\".", s)),
-        }
-
-        Ok(CFGRule {
-            head: head,
-            composition : composition,
-            weight: weight,
-        })
-    }
-}
-
-impl<N: FromStr, T: FromStr + Clone> FromStr for Composition<N,T> {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut buffer: String= String::new();
-        let mut composition: Vec<LetterT<N,T>> = Vec::new();
-
-        let mut active: bool = false;
-
-        for c in s.chars() {
-            match (c, active) {
-                ('[', false) => active = true,
-                (']', true) => {
-                    return Ok(Composition { composition: composition });
-                },
-                ('\t',_) => (),
-                (' ',_) => (),
-
-                (_ ,true) => {
-                    buffer.push(c);
-                    composition.push(try!(buffer.trim().parse()));
-                    buffer.clear();
-                }
-                _ => (),
-            }
-        }
-        Err(format!("Can not parse \"{}\" as Composition<T>.", s))
-    }
-}
-
-impl<N: FromStr, T: FromStr> FromStr for LetterT<N,T> {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let c= s.chars().nth(0);
-        match c {
-             Some(x) if !x.is_lowercase() => {
-                Ok(LetterT::Label(try!(s.parse()
-                                 .map_err(|_| format!("Could not parse \"{}\" as T.", x)))))
-            }
-            Some(x) if x.is_lowercase() => {
-                Ok(LetterT::Value(try!(s.parse()
-                    .map_err(|_| format!("Could not parse \"{}\" as T.", x)))))
-            }
-            Some(_) => Err(format!("Could not parse \"{}\" as LetterT<T>.", s)),
-
-            None => Err(format!("Could not parse \"{}\" as LetterT<T>.", s)),
-        }
     }
 }
