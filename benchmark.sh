d1bf5dd2984d998703876b2bb5a1d0d98311b3c6
Nogodful <kornmaxhohnstein@gmail.com>
2017-09-27 08:21:01 +0200

    Small changes to benchmark setup

--- benchmark.sh
+++ benchmark.sh
@@ -44,6 +44,38 @@ while read -r n; do
 done < corp/bench_no_nfa.list
 
 while read -r n; do
+  make bench -B NAME=corp/$n PTK=10 WORDS=3 NFA=true EQC=corp/$n
+  mkdir res/$n-10
+  cp benchmark-results.txt res/$n-10/results.txt
+  cp benchmark.log res/$n-10/results.log
+
+  make bench -B NAME=corp/$n PTK=10 WORDS=3 NFA=true EQC=corp/${n}_2
+  mkdir res/$n-10-2
+  cp benchmark-results.txt res/$n-10-2/results.txt
+  cp benchmark.log res/$n-10-2/results.log
+
+  make bench -B NAME=corp/$n PTK=15 WORDS=3 NFA=true EQC=corp/$n
+  mkdir res/$n-15
+  cp benchmark-results.txt res/$n-15/results.txt
+  cp benchmark.log res/$n-15/results.log
+
+  make bench -B NAME=corp/$n PTK=15 WORDS=3 NFA=true EQC=corp/${n}_2
+  mkdir res/$n-15-2
+  cp benchmark-results.txt res/$n-15-2/results.txt
+  cp benchmark.log res/$n-15-2/results.log
+
+  make bench -B NAME=corp/$n PTK=20 WORDS=3 NFA=true EQC=corp/$n
+  mkdir res/$n-20
+  cp benchmark-results.txt res/$n-20/results.txt
+  cp benchmark.log res/$n-20/results.log
+
+  make bench -B NAME=corp/$n PTK=20 WORDS=3 NFA=true EQC=corp/${n}_2
+  mkdir res/$n-20-2
+  cp benchmark-results.txt res/$n-20-2/results.txt
+  cp benchmark.log res/$n-20-2/results.log
+done < corp/bench_no_nfa_weak.list
+
+while read -r n; do
   make bench -B NAME=corp/$n PTK=5 WORDS=3 EQC=corp/$n
   mkdir res/$n-nfa-05
   cp benchmark-results.txt res/$n-nfa-05/results.txt
@@ -63,4 +95,4 @@ while read -r n; do
   mkdir res/$n-nfa-10-2
   cp benchmark-results.txt res/$n-nfa-10-2/results.txt
   cp benchmark.log res/$n-nfa-10-2/results.log
-done < corp/bench-nfa.list
+done < corp/bench_nfa.list
