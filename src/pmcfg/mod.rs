49a0ff01fe6d3bb3a8b0a96e70135bab4d939a6c
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-01-18 14:36:50 +0100

    src/pmcfg/mod.rs: added test for identify_terminals()

--- src/pmcfg/mod.rs
+++ src/pmcfg/mod.rs
@@ -316,4 +316,39 @@ mod tests {
 
         evaluate(&term_map);
     }
+
+    #[test]
+    fn test_identify_terminals() {
+        let mut tree_map = BTreeMap::new();
+        tree_map.insert(vec![], Composition::from(vec![
+            vec![Var(0,0), T("a"), Var(0,1), T("b")]
+        ]));
+        tree_map.insert(vec![0], Composition::from(vec![
+            vec![Var(1,0)],
+            vec![T("c")]
+        ]));
+        tree_map.insert(vec![1,0], Composition::from(vec![
+            vec![T("d")]
+        ]));
+
+        let mut identified_tree_map = BTreeMap::new();
+        identified_tree_map.insert(vec![], Composition::from(vec![
+            vec![Var(0,0), T((vec![], 1)), Var(0,1), T((vec![], 3))]
+        ]));
+        identified_tree_map.insert(vec![0], Composition::from(vec![
+            vec![Var(1,0)],
+            vec![T((vec![0], 1))]
+        ]));
+        identified_tree_map.insert(vec![1,0], Composition::from(vec![
+            vec![T((vec![1,0], 0))]
+        ]));
+
+        let mut terminal_map = BTreeMap::new();
+        terminal_map.insert((vec![], 1), "a");
+        terminal_map.insert((vec![], 3), "b");
+        terminal_map.insert((vec![0], 1), "c");
+        terminal_map.insert((vec![1,0], 0), "d");
+
+        assert_eq!((identified_tree_map, terminal_map), identify_terminals(&tree_map));
+    }
 }
