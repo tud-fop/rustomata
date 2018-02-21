1b551a554ee584fd60f15e49e600b595f534ed14
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2018-02-21 15:36:45 +0100

    src/lib.rs: added missing file

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+extern crate integeriser;
+extern crate log_domain;
+#[macro_use]
+extern crate nom;
+extern crate num_traits;
+extern crate time;
+extern crate rand;
+
+pub mod approximation;
+#[macro_use]
+pub mod recognisable;
+pub mod cfg;
+pub mod nfa;
+pub mod pmcfg;
+pub mod push_down_automaton;
+pub mod tree_stack_automaton;
+mod util;
+
+#[cfg(test)]
+mod tests;
