8dd18075d5718c0bba1fbcdc49ac7e4b43d48380
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-02-28 12:19:43 +0100

    src/util/tree.rs: added IntoIterator for mutable GornTree

--- src/util/tree.rs
+++ src/util/tree.rs
@@ -1,13 +1,15 @@
 use std::collections::BTreeMap;
-use std::collections::btree_map::{IntoIter, Iter};
+use std::collections::btree_map;
 use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Eq)]
-pub struct GornTree<V>(BTreeMap<Vec<usize>, V>);
+pub struct GornTree<V> {
+    map: BTreeMap<Vec<usize>, V>,
+}
 
 impl<V> GornTree<V> {
     pub fn new() -> GornTree<V> {
-        GornTree(BTreeMap::new())
+        GornTree { map: BTreeMap::new() }
     }
 }
 
@@ -15,36 +17,45 @@ impl<V> Deref for GornTree<V> {
     type Target = BTreeMap<Vec<usize>, V>;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.map
     }
 }
 
 impl<V> DerefMut for GornTree<V> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        &mut self.map
     }
 }
 
 impl<V> IntoIterator for GornTree<V> {
     type Item = (Vec<usize>, V);
-    type IntoIter = IntoIter<Vec<usize>, V>;
+    type IntoIter = btree_map::IntoIter<Vec<usize>, V>;
 
-    fn into_iter(self) -> IntoIter<Vec<usize>, V> {
-        self.0.into_iter()
+    fn into_iter(self) -> btree_map::IntoIter<Vec<usize>, V> {
+        self.map.into_iter()
     }
 }
 
 impl<'a, V> IntoIterator for &'a GornTree<V> {
     type Item = (&'a Vec<usize>, &'a V);
-    type IntoIter = Iter<'a, Vec<usize>, V>;
+    type IntoIter = btree_map::Iter<'a, Vec<usize>, V>;
 
-    fn into_iter(self) -> Iter<'a, Vec<usize>, V> {
-        (&self.0).into_iter()
+    fn into_iter(self) -> btree_map::Iter<'a, Vec<usize>, V> {
+        (&self.map).into_iter()
+    }
+}
+
+impl<'a, V> IntoIterator for &'a mut GornTree<V> {
+    type Item = (&'a Vec<usize>, &'a mut V);
+    type IntoIter = btree_map::IterMut<'a, Vec<usize>, V>;
+
+    fn into_iter(self) -> btree_map::IterMut<'a, Vec<usize>, V> {
+        (&mut self.map).into_iter()
     }
 }
 
 impl<V: PartialEq> PartialEq for GornTree<V> {
     fn eq(&self, other: &GornTree<V>) -> bool {
-        self.0.eq(&other.0)
+        self.map.eq(&other.map)
     }
 }
