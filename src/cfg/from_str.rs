2a41706e7b54b274fc4fb7574c6b399ad04f2cfc
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-03-16 09:41:40 +0100

    src/cfg/from_str.rs: added tests

--- src/cfg/from_str.rs
+++ src/cfg/from_str.rs
@@ -1,11 +1,10 @@
+use nom::{IResult, is_space};
+use num_traits::One;
 use std::fmt::Debug;
 use std::marker::PhantomData;
 use std::str::{FromStr, from_utf8};
 
-use nom::{IResult, is_space};
-use num_traits::One;
-
-use cfg::{LetterT, CFGComposition, CFGRule, CFG};
+use cfg::{CFG, CFGComposition, CFGRule, LetterT};
 use util::parsing::*;
 
 impl<N, T, W> FromStr for CFG<N, T, W>
@@ -168,5 +167,120 @@ pub mod tests {
                        S → [T a]\n\
                        % trailing comment";
         let _: CFG<char, char, usize> = grammar.parse().unwrap();
+    }
+
+    #[test]
+    fn test_parse_letter_t_legal_input() {
+        let legal_inputs = vec![
+            ("Nt S xyz", " xyz", LetterT::Label('S')),
+            ("Nt  S", "", LetterT::Label('S')),
+            ("T a xyz", " xyz", LetterT::Value('a')),
+        ];
+
+        for (legal_input, control_rest, control_parsed) in legal_inputs {
+            assert_eq!(
+                (control_rest.as_bytes(), control_parsed),
+                parse_letter_t::<char, char>(legal_input.as_bytes()).unwrap()
+            );
+        }
+    }
+
+    #[test]
+    fn test_parse_letter_t_illegal_input() {
+        let illegal_inputs = vec![
+            " Nt 1",
+            "nt 1",
+            "t 1",
+            "Nt:1",
+            "Nt a",
+            "T a",
+        ];
+
+        for illegal_input in illegal_inputs {
+            match parse_letter_t::<u8, u8>(illegal_input.as_bytes()) {
+                IResult::Done(_, _) | IResult::Incomplete(_) =>
+                    panic!("Was able to parse the illegal input \'{}\'", illegal_input),
+                IResult::Error(_) => (),
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_letter_t_incomplete_input() {
+        let incomplete_inputs = vec![
+            "Nt",
+            "T",
+        ];
+
+        for incomplete_input in incomplete_inputs {
+            match parse_letter_t::<char, char>(incomplete_input.as_bytes()) {
+                IResult::Done(_, _) | IResult::Error(_) =>
+                    panic!("The input was not handled as incomplete: \'{}\'", incomplete_input),
+                IResult::Incomplete(_) => (),
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_cfg_rule_legal_input() {
+        let rule = CFGRule {
+            head: 'S',
+            composition: CFGComposition { composition: vec![LetterT::Value('a')] },
+            weight: 1.0
+        };
+        let legal_inputs = vec![
+            ("S → [T a] # 1 % comment", "", rule.clone()),
+            ("S  →    [T a]#1 %comment", "", rule.clone()),
+            ("S → [T a] # 1.0", "", rule.clone()),
+            ("S → [T a]", "", rule.clone()),
+            ("S → [T a] % comment", "", rule.clone()),
+            ("S -> [T a] # 1", "", rule.clone()),
+            ("S => [T a] # 1", "", rule.clone()),
+        ];
+
+        for (legal_input, control_rest, control_parsed) in legal_inputs {
+            assert_eq!(
+                (control_rest.as_bytes(), control_parsed),
+                parse_cfg_rule::<char, char, f32>(legal_input.as_bytes()).unwrap()
+            );
+        }
+    }
+
+    #[test]
+    fn test_parse_cfg_rule_illegal_input() {
+        let illegal_inputs = vec![
+            " S → [T a] # 1 % comment",
+            "S [T a] # 1 % comment",
+            "S → [T a] # 1 comment",
+            "S ~> [T a] # 1",
+            "AB → [T a] # 1 % comment",
+            "S → [T a] # a % comment",
+            "S → [T a] #",
+        ];
+
+        for illegal_input in illegal_inputs {
+            match parse_cfg_rule::<char, char, f32>(illegal_input.as_bytes()) {
+                IResult::Done(_, _) | IResult::Incomplete(_) =>
+                    panic!("Was able to parse the illegal input \'{}\'", illegal_input),
+                IResult::Error(_) => (),
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_cfg_rule_incomplete_input() {
+        let incomplete_inputs = vec![
+            "S →",
+            "S",
+        ];
+
+        for incomplete_input in incomplete_inputs {
+            match parse_cfg_rule::<char, char, f32>(incomplete_input.as_bytes()) {
+                IResult::Done(_, output) =>
+                    panic!("The input was not handled as incomplete: \'{}\'", output),
+                IResult::Error(error) => panic!("Error with \'{}\'; {:?}", incomplete_input, error),
+                IResult::Incomplete(_) => (),
+            }
+        }
     }
 }
