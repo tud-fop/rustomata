a52bc179f41f9039f037ee8c7620fbf4cac88861
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-03-27 09:57:50 +0200

    tests/pmcfg.rs: reworked integration test of TreeStackAutomaton::from_str()

--- tests/pmcfg.rs
+++ tests/pmcfg.rs
@@ -3,6 +3,7 @@ extern crate log_domain;
 extern crate rustomata;
 
 use log_domain::LogDomain;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 use std::rc::Rc;
@@ -52,97 +53,6 @@ fn test_example_pmcfg_to_negra() {
     );
 
     assert_eq!(negra_control_string, negra_string);
-}
-
-#[test]
-fn test_from_str_automaton() {
-    let i1: TreeStackInstruction<String> = TreeStackInstruction::Up {
-        n: 1,
-        current_val: "zwei".to_string(),
-        old_val: "drei".to_string(),
-        new_val: "vier".to_string(),
-    };
-
-    let i2: TreeStackInstruction<String> = TreeStackInstruction::Push {
-        n: 1,
-        current_val: "zwei".to_string(),
-        new_val: "vier".to_string(),
-    };
-
-    let i3: TreeStackInstruction<String> = TreeStackInstruction::Down {
-        current_val: "zwei".to_string(),
-        old_val: "drei".to_string(),
-        new_val: "vier".to_string(),
-    };
-
-    let i1_string = "Up 1 zwei drei vier";
-    let i1_string2 = "   Up 1  zwei  drei   vier  ";
-    let i2_string = "Push 1 zwei vier";
-    let i3_string = "Down zwei drei vier";
-
-    assert_eq!(Ok(i1.clone()), i1_string.parse());
-    assert_eq!(Ok(i1.clone()), i1_string2.parse());
-    assert_eq!(Ok(i2.clone()), i2_string.parse());
-    assert_eq!(Ok(i3.clone()), i3_string.parse());
-
-    let t1 = Transition {
-        word: vec!["hello".to_string(), "world".to_string()],
-        weight: LogDomain::new(0.5).unwrap(),
-        instruction: i1,
-    };
-
-    let t2 = Transition {
-        word: vec!["\"hello\\".to_string(), "world".to_string()],
-        weight: LogDomain::new(0.5).unwrap(),
-        instruction: i2,
-    };
-
-    let t3 = Transition {
-        word: vec!["\"hello\\".to_string(), "world".to_string()],
-        weight: LogDomain::new(0.5).unwrap(),
-        instruction: i3,
-    };
-
-    let mut t1_string = String::from("Transition [\"hello\",\"world\"] (");
-    t1_string.push_str(i1_string);
-    t1_string.push_str(") #0.5");
-
-    let mut t2_string = String::from("Transition [\"\\\"hello\\\\\",\"world\"] (");
-    t2_string.push_str(i2_string);
-    t2_string.push_str(") #0.5");
-
-    let mut t3_string = String::from("Transition [\"\\\"hello\\\\\",\"world\"] (");
-    t3_string.push_str(i3_string);
-    t3_string.push_str(") #0.5");
-
-    assert_eq!(Ok(t1.clone()), t1_string.parse());
-    assert_eq!(Ok(t2.clone()), t2_string.parse());
-    assert_eq!(Ok(t3.clone()), t3_string.parse());
-
-    let automaton: TreeStackAutomaton<String, String, LogDomain<f64>> = TreeStackAutomaton::new(
-        vec![t1.clone(), t2.clone(), t3.clone()],
-        TreeStack::new("eins".to_string())
-    );
-
-    let mut automaton_string: String = String::from("initial: eins\n\n");
-    automaton_string.push_str(t1_string.as_str());
-    automaton_string.push_str("\n");
-    automaton_string.push_str(t2_string.as_str());
-    automaton_string.push_str("\n");
-    automaton_string.push_str(t3_string.as_str());
-    automaton_string.push_str("\n");
-
-    let automaton_parse: TreeStackAutomaton<String, String, LogDomain<f64>> = automaton_string.parse().unwrap();
-
-    let ts1: Vec<_> = automaton.list_transitions().collect();
-    let ts2: Vec<_> = automaton_parse.list_transitions().collect();
-
-    assert_eq!(ts1, ts2);
-
-    assert_eq!(
-        automaton.initial(),
-        automaton_parse.initial()
-    );
 }
 
 #[test]
@@ -279,4 +189,70 @@ fn test_pmcfg_from_str_correctness() {
             automaton.recognise(input).next().is_some()
         );
     }
+}
+
+#[test]
+fn test_tree_stack_automaton_from_str() {
+    use TreeStackInstruction::{Up, Down, Push};
+
+    let unprocessed_transitions = vec![
+        ("",  Push { n: 0, current_val: 1.to_string(), new_val: 2.to_string() },                            1.0),
+        ("",  Push { n: 0, current_val: 1.to_string(), new_val: 3.to_string() },                            1.0),
+        ("",  Down { current_val: 4.to_string(), old_val: 18.to_string(), new_val: 19.to_string() },        1.0),
+        ("",  Down { current_val: 5.to_string(), old_val: 6.to_string(), new_val: 5.to_string() },          1.0),
+        ("",  Down { current_val: 5.to_string(), old_val: 7.to_string(), new_val: 8.to_string() },          1.0),
+        ("d", Up   { n: 0, current_val: 9.to_string(), old_val: 10.to_string(), new_val: 11.to_string() },  1.0),
+        ("d", Up   { n: 0, current_val: 9.to_string(), old_val: 5.to_string(), new_val: 9.to_string() },    1.0),
+        ("",  Up   { n: 0, current_val: 12.to_string(), old_val: 2.to_string(), new_val: 13.to_string() },  1.0),
+        ("",  Up   { n: 0, current_val: 12.to_string(), old_val: 14.to_string(), new_val: 15.to_string() }, 1.0),
+        ("c", Up   { n: 0, current_val: 6.to_string(), old_val: 13.to_string(), new_val: 10.to_string() },  1.0),
+        ("c", Up   { n: 0, current_val: 6.to_string(), old_val: 16.to_string(), new_val: 6.to_string() },   1.0),
+        ("",  Down { current_val: 11.to_string(), old_val: 9.to_string(), new_val: 17.to_string() },        1.0),
+        ("",  Down { current_val: 11.to_string(), old_val: 8.to_string(), new_val: 4.to_string() },         1.0),
+        ("b", Up   { n: 0, current_val: 15.to_string(), old_val: 2.to_string(), new_val: 13.to_string() },  1.0),
+        ("b", Up   { n: 0, current_val: 15.to_string(), old_val: 14.to_string(), new_val: 15.to_string() }, 1.0),
+        ("",  Push { n: 0, current_val: 18.to_string(), new_val: 1.to_string() },                           1.0),
+        ("",  Down { current_val: 17.to_string(), old_val: 9.to_string(), new_val: 17.to_string() },        1.0),
+        ("",  Down { current_val: 17.to_string(), old_val: 8.to_string(), new_val: 4.to_string() },         1.0),
+        ("",  Down { current_val: 2.to_string(), old_val: 3.to_string(), new_val: 14.to_string() },         1.0),
+        ("",  Down { current_val: 2.to_string(), old_val: 1.to_string(), new_val: 12.to_string() },         1.0),
+        ("",  Up   { n: 0, current_val: 8.to_string(), old_val: 10.to_string(), new_val: 11.to_string() },  1.0),
+        ("",  Up   { n: 0, current_val: 8.to_string(), old_val: 5.to_string(), new_val: 9.to_string() },    1.0),
+        ("a", Push { n: 0, current_val: 3.to_string(), new_val: 2.to_string() },                            1.0),
+        ("a", Push { n: 0, current_val: 3.to_string(), new_val: 3.to_string() },                            1.0),
+        ("",  Down { current_val: 13.to_string(), old_val: 15.to_string(), new_val: 16.to_string() },       1.0),
+        ("",  Down { current_val: 13.to_string(), old_val: 12.to_string(), new_val: 7.to_string() },        1.0),
+        ("",  Down { current_val: 10.to_string(), old_val: 6.to_string(), new_val: 5.to_string() },         1.0),
+        ("",  Down { current_val: 10.to_string(), old_val: 7.to_string(), new_val: 8.to_string() },         1.0),
+        ("",  Up   { n: 0, current_val: 7.to_string(), old_val: 13.to_string(), new_val: 10.to_string() },  1.0),
+        ("",  Up   { n: 0, current_val: 7.to_string(), old_val: 16.to_string(), new_val: 6.to_string() },   1.0),
+        ("",  Down { current_val: 14.to_string(), old_val: 3.to_string(), new_val: 14.to_string() },        1.0),
+        ("",  Down { current_val: 14.to_string(), old_val: 1.to_string(), new_val: 12.to_string() },        1.0),
+        ("",  Down { current_val: 16.to_string(), old_val: 15.to_string(), new_val: 16.to_string() },       1.0),
+        ("",  Down { current_val: 16.to_string(), old_val: 12.to_string(), new_val: 7.to_string() },        1.0),
+    ];
+
+    let mut transitions = Vec::new();
+    for (terminal, instruction, weight) in unprocessed_transitions {
+        transitions.push(Transition {
+            word: terminal.chars().map(|x| x.to_string()).collect(),
+            instruction,
+            weight: LogDomain::new(weight).unwrap()
+        });
+    }
+    let control_automaton = TreeStackAutomaton::new(transitions, TreeStack::new(18.to_string()));
+
+    let mut automaton_file = File::open("examples/example.tsa").unwrap();
+    let mut automaton_string = String::new();
+    let _ = automaton_file.read_to_string(&mut automaton_string);
+    let automaton: TreeStackAutomaton<String, String, LogDomain<f64>> = automaton_string.parse().unwrap();
+
+    assert_eq!(
+        control_automaton.initial(),
+        automaton.initial()
+    );
+    assert_eq!(
+        control_automaton.list_transitions().collect::<HashSet<_>>(),
+        automaton.list_transitions().collect::<HashSet<_>>()
+    );
 }
